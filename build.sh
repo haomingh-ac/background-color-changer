c3a38f0887035c6d1752220d509439073caa6b13
Darin Kadrioski <darin@nebiru.com>
2017-07-12 17:50:00 -0700

    Included by mistake :-|

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-echo 'Copying files into testbed...';
-npm patch
-cp index.js ../bg-color-test/node_modules/background-color-changer
-cp package.json ../bg-color-test/node_modules/background-color-changer
